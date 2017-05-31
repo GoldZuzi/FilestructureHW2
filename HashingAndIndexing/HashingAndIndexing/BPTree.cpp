5a66d3d9107a78506b9ccd7abd42df387cbb6468
GoldZuzi <yeonmu.lee@gmail.com>
2017-05-31 22:26:49 +0900

    기존에는 Node의 원소 중 해당 노드가 leaf노드인지 아닌지 구별하는 boolean 속성이 있었는데, 제거 하였다. 또한, 몇 개의 데이터를 넣어서 실행시키며 결과를 확인하여 함수 내 오류 원인을 제거하였다.

--- HashingAndIndexing/HashingAndIndexing/BPTree.cpp
+++ HashingAndIndexing/HashingAndIndexing/BPTree.cpp
@@ -1,52 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <vector>
-using namespace std;
+#include "BPTree.h"
 
-//struct leafNode;
-//struct internalNode;
-struct entry;
-struct node;
+int depth;
 
-node *root;
-
-#define ENTRYSIZE 6
-#define Fanout ENTRYSIZE + 1;
-
-
-typedef union blockPointer {
-	int blockNum;
-	node *childNode;
-};
-
-struct entry
-{
-	float score;
-	blockPointer a;
-};
-
-struct node
-{
-	bool isLeaf;
-	int entryCount;
-	entry nodeEntry[ENTRYSIZE];
-	node *elseNode; //entry 내의 모든 key값보다 큰 key 값들이 저장된 노드의 포인터
-};
-
-node* createNode(bool isLeaf) {
+node* createNode() {
 	node *newNode;
 	newNode = (struct node *)malloc(sizeof(struct node));
 	memset(newNode, 0, sizeof(struct node));
-	newNode->isLeaf = isLeaf;
+	//newNode->isLeaf = isLeaf;
 	//newNode->parentNode = parent;
 	return newNode;
 }
 
 void initNode(node *initialNode) {
-	initialNode->isLeaf = true;
+	//initialNode->isLeaf = true;
 	initialNode->elseNode = NULL;
 	initialNode->entryCount = 0;
+	depth = 0;
 }
 
 void copyNodeEntry(node *dst, node *src, int dstFirst, int srcFirst, int size) {
@@ -54,7 +23,7 @@ void copyNodeEntry(node *dst, node *src, int dstFirst, int srcFirst, int size) {
 	memcpy(&dst->nodeEntry[dstFirst], &src->nodeEntry[srcFirst], size);
 }
 
-static struct node* split(node *nodeToSplit, float key, vector<node*> &parentSet, int parentIndex) {
+static struct node* split(node *nodeToSplit, float key, vector<node*> &parentSet, int parentIndex, bool isLeaf) {
 	int splitFactor = (ENTRYSIZE >> 1) - 1;
 	float splitKey = nodeToSplit->nodeEntry[splitFactor].score;
 	
@@ -66,24 +35,26 @@ static struct node* split(node *nodeToSplit, float key, vector<node*> &parentSet
 	rightNode는 새로 생성하여 leftNode의
 	split 지점 이후의 원소들을 복사함*/
 	leftNode = nodeToSplit;	//
-	rightNode = createNode(nodeToSplit->isLeaf);	
+	//rightNode = createNode(nodeToSplit->isLeaf);	
+	rightNode = createNode();
 	copyNodeEntry(rightNode, nodeToSplit, 0, splitFactor + 1, ENTRYSIZE - splitFactor + 1);
 		
-	if (leftNode->isLeaf) leftNode->elseNode = rightNode;
+	if (isLeaf) leftNode->elseNode = rightNode;
 	else leftNode->elseNode = leftNode->nodeEntry[splitFactor + 1].a.childNode;
 	
 	/*rightNode로 복사된 원소들을 leftNode에서 제거함*/
 	for (int i = splitFactor + 1; i < ENTRYSIZE; i++) {
-		free(&leftNode->nodeEntry[i]);
+		memset(&leftNode->nodeEntry[i], 0, sizeof(struct entry));
 	}
 
 	leftNode->entryCount = splitFactor + 1;
-	rightNode->entryCount = ENTRYSIZE - splitFactor + 1;
+	rightNode->entryCount = ENTRYSIZE - (splitFactor + 1);
 	
 	/*ParentNode를 지정하는 과정*/
 	if (parentSet[parentIndex] == NULL) {   //분할되는 노드가 기존의 루트노드였을 경우
-		parentNode = createNode(false); //ParentNode 생성
+		parentNode = createNode(); //ParentNode 생성
 		root = parentNode;
+		depth++;
 	}
 	
 	else { //ParentNode는 기존 노드의 상위 노드
@@ -91,7 +62,7 @@ static struct node* split(node *nodeToSplit, float key, vector<node*> &parentSet
 		
 		/*parentNode가 꽉 찼을 경우 스플릿*/
 		if (parentNode->entryCount == ENTRYSIZE) {
-			parentNode = split(parentNode, splitKey, parentSet, parentIndex - 1);
+			parentNode = split(parentNode, splitKey, parentSet, parentIndex - 1, false);
 		}
 	}
 	
@@ -109,7 +80,8 @@ void internalNodeInsert(node *internalNode, float key, node *leftNode, node *rig
 	/*삽입될 엔트리가 가리키는 포인터는 왼쪽 child 노드*/
 	ptr.childNode = leftNode;
 	/*엔트리 생성*/
-	createNewEntry(&newEntry, internalNode->isLeaf, key, ptr);
+	//createNewEntry(&newEntry, internalNode->isLeaf, key, ptr);
+	createNewEntry(&newEntry, false, key, ptr);
 	int index = internalNode->entryCount;
 	for (int i = 0; i < internalNode->entryCount; i++) {
 		if (key <= internalNode->nodeEntry[i].score) {
@@ -142,17 +114,27 @@ int search(float key) {
 int searchDetail(node *currentNode, float key)
 {
 	int count = 0;
+	int currentDepth = 0;
 	while (1) {
-		if (currentNode->isLeaf) break;
+		//if (currentNode->isLeaf) break;
+		if (currentDepth == depth) break;
 		if (currentNode->entryCount > 0) {
 			for (int i = 0; i < currentNode->entryCount; i++) {
 				count++;
 				if (key <= currentNode->nodeEntry[i].score) {
+					//if (currentNode->isLeaf) break;
+					if (currentDepth == depth) break;
 					currentNode = currentNode->nodeEntry[i].a.childNode;
+					currentDepth++;
 					break;
 				}
-				if (count == currentNode->entryCount)
+				if (count == currentNode->entryCount) {
+					//if (currentNode->isLeaf) break;
+					if (currentDepth == depth) break;
 					currentNode = currentNode->elseNode;
+					currentDepth++;
+					break;
+				}
 			}
 			count = 0;
 		}
@@ -179,10 +161,11 @@ void createNewEntry(entry *newEntry, bool isLeaf, float key, blockPointer ptr) {
 bool insert(float key, blockPointer ptr) {
 	entry newEntry;
 	node *tmpNode = root;
-	int count;
+	int count = 0;
 	int index;
 	vector<node*> parentSet;
 	node *parentNode = NULL;
+	int currentDepth = 0;
 
 	/*key값이 이미 트리에 존재하는지 체크*/
 	if (search(key) >= 0) {
@@ -192,17 +175,25 @@ bool insert(float key, blockPointer ptr) {
 	/*엔트리를 삽입할 리프노드를 찾는 단계*/
 	while (1) {
 		parentSet.push_back(parentNode);
-		if (tmpNode->isLeaf) break;
+		//if (tmpNode->isLeaf) break;
+		if (currentDepth == depth) break;
 		if (tmpNode->entryCount > 0) {
 			for (int i = 0; i < tmpNode->entryCount; i++) {
 				count++;
 				if (key <= tmpNode->nodeEntry[i].score) {
 					parentNode = tmpNode;
 					tmpNode = tmpNode->nodeEntry[i].a.childNode;
+					currentDepth++;
 					break;
 				}
-				if (count == tmpNode->entryCount)
+				if (count == tmpNode->entryCount) {
+					//if (tmpNode->isLeaf) break;
+					if (currentDepth == depth) break;
+					parentNode = tmpNode;
 					tmpNode = tmpNode->elseNode;
+					currentDepth++;
+					break;
+				}
 			}
 			count = 0;
 		}
@@ -210,11 +201,11 @@ bool insert(float key, blockPointer ptr) {
 
 	/*해당 리프가 꽉 찼을 경우 노드 분할*/
 	if (tmpNode->entryCount == ENTRYSIZE) {
-		tmpNode = split(tmpNode, key, parentSet, parentSet.size() - 1);
+		tmpNode = split(tmpNode, key, parentSet, parentSet.size() - 1, true);
 	}
 
 	/*새로운 엔트리 생성*/
-	createNewEntry(&newEntry, tmpNode->isLeaf, key, ptr);
+	createNewEntry(&newEntry, true, key, ptr);
 	
 	/*노드에 삽입할 위치 지정*/
 	index = tmpNode->entryCount;
@@ -225,7 +216,7 @@ bool insert(float key, blockPointer ptr) {
 		}
 	}
 	/*노드에 엔트리를 삽입*/
-	if (tmpNode->isLeaf) { 
+	//if (tmpNode->isLeaf) { 
 		/*노드가 리프일 경우(무조건 리프일듯)*/		
 		for (int i = tmpNode->entryCount; i > index; i--) {
 			memcpy(&tmpNode->nodeEntry[i], &tmpNode->nodeEntry[i - 1], sizeof(struct entry)); //엔트리를 하나씩 right shift
@@ -233,7 +224,7 @@ bool insert(float key, blockPointer ptr) {
 		memset(&tmpNode->nodeEntry[index], 0, sizeof(struct entry)); //삽입할 엔트리 번지 초기화
 		memcpy(&tmpNode->nodeEntry[index], &newEntry, sizeof(struct entry)); //엔트리 삽입
 		tmpNode->entryCount++;
-	}
+	//}
 	return true;
 }
 
--- HashingAndIndexing/HashingAndIndexing/main.cpp
+++ HashingAndIndexing/HashingAndIndexing/main.cpp
@@ -1,5 +1,58 @@
+#include "BPTree.h"
 
+node *root = createNode();
 
 int main() {
+	initNode(root);
+	int b = 0;
+	blockPointer a;
+	a.blockNum = 4;
+	insert(3.3, a);
+	a.blockNum = 5;
+	insert(3.4, a);
+	a.blockNum = 6;
+	insert(3.5, a);
+
+	a.blockNum = 7;
+	insert(3.6, a);
+	a.blockNum = 8;
+	insert(3.7, a);
+	a.blockNum = 9;
+	insert(3.8, a);
+
+	a.blockNum = 10;
+	insert(3.9, a);
+	a.blockNum = 11;
+	insert(4.0, a);
+	a.blockNum = 12;
+	insert(3.55, a);
+
+	a.blockNum = 17;
+	insert(3.42, a);
+	a.blockNum = 20;
+	insert(3.58, a);
+
+	b = search(3.3);
+	printf("%d ", b);
+	b = search(3.4);
+	printf("%d ", b);
+	b = search(3.42);
+	printf("%d ", b);
+	b = search(3.5);
+	printf("%d ", b);
+	b = search(3.55);
+	printf("%d ", b);
+	b = search(3.58);
+	printf("%d ", b);
+	b = search(3.6);
+	printf("%d ", b);
+	b = search(3.7);
+	printf("%d ", b);
+	b = search(3.8);
+	printf("%d ", b);
+	b = search(3.9);
+	printf("%d ", b);
+	b = search(4.0);
+	printf("%d ", b);
 	return 0;
 }
